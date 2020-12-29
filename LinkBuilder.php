32943f673697f8cac3c82d1e1eb8a7074174871b
Jaime Rodriguez <hi.i.am.jaime@gmail.com>
2020-12-29 00:58:16 -0800

    update: add the ability to get link, title, and target

--- LinkBuilder.php
+++ LinkBuilder.php
@@ -34,7 +34,10 @@ class LinkBuilder extends Module
      */
     public $hooks = [
         'linkbuilder_preprocess'    => 'setup',
-        'render_placeholder_smlink' => 'createLink'
+        'render_placeholder_smlink' => 'createLink',
+        'render_placeholder_smlink.title' => 'getTitle',
+        'render_placeholder_smlink.link' => 'getLink',
+        'render_placeholder_smlink.target' => 'getTarget',
     ];
 
     /**
@@ -57,11 +60,58 @@ class LinkBuilder extends Module
     {
         $args = func_get_args();
         $page_id = $args[1];
-        $title = isset($args[2]) ? $args[2] : $this->sitemap->title($page_id);
+        // Remove the first two arguments
+        array_shift($args);
+        array_shift($args);
+
+        // Clean surrounding quotes
+        $title = count($args) ? trim(trim(implode(" ", $args), '"'), "'") : $this->sitemap->title($page_id);
         $link = $this->sitemap->link($page_id);
         $target = $this->sitemap->target($page_id);
 
         return "<a href=\"{$link}\" target=\"{$target}\">{$title}</a>";
+    }
+
+    /**
+     * Starts the timer when the framework loads.
+     *
+     * @return void
+     */
+    public function getLink()
+    {
+        $args = func_get_args();
+        $page_id = $args[1];
+        return $this->sitemap->link($page_id);
+    }
+
+    /**
+     * Starts the timer when the framework loads.
+     *
+     * @return void
+     */
+    public function getTarget()
+    {
+        $args = func_get_args();
+        $page_id = $args[1];
+        return $this->sitemap->target($page_id);
+    }
+
+    /**
+     * Starts the timer when the framework loads.
+     *
+     * @return void
+     */
+    public function getTitle()
+    {
+        $args = func_get_args();
+        $page_id = $args[1];
+        
+        // Remove the first two arguments
+        array_shift($args);
+        array_shift($args);
+
+        // Clean surrounding quotes
+        return count($args) ? trim(trim(implode(" ", $args), '"'), "'") : $this->sitemap->title($page_id);
     }
 }
 
